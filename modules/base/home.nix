b6d6bbcfd6bf08217f052eb0367cebd778b088c8
altacountbabi <altacountbabi@users.noreply.github.com>
2025-10-18 15:02:24 +0300

    use systemd-tmpfiles in hjem rather than smfh linker

--- modules/base/home.nix
+++ modules/base/home.nix
@@ -4,24 +4,17 @@
   flake.nixosModules.base =
     {
       config,
-      pkgs,
       ...
     }:
     {
       imports = [ inputs.hjem.nixosModules.default ];
 
       config = {
-        environment.systemPackages = [
-          inputs.hjem.packages.${pkgs.stdenv.system}.smfh
-        ];
-
         hjem =
           let
             username = config.prefs.user.name;
           in
           {
-            linker = inputs.hjem.packages.${pkgs.stdenv.system}.smfh;
-
             users.${username} = {
               enable = true;
               directory = "/home/${username}";
