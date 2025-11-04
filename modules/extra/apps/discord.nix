1cb2c9e261eae43a60a81b2eac9b49cae15a3acb
altacountbabi <altacountbabi@users.noreply.github.com>
2025-11-04 12:20:22 +0200

    Use `proot` wrapper for discord rather than hjem. This does disable the sandbox for discord as it uses ptrace, which conflicts with proot.

--- modules/extra/apps/discord.nix
+++ modules/extra/apps/discord.nix
@@ -11,6 +11,7 @@
       options.prefs = {
         discord.package = mkOpt types.package (inputs.nixcord.packages.${pkgs.system}.discord.override {
           withVencord = true;
+          withOpenASAR = true;
           commandLineArgs = "--enable-features=UseOzonePlatform --ozone-platform=wayland";
         }) "The package to use for discord";
         discord.autostart = mkOpt types.bool false "Whether to automatically start discord at startup";
@@ -20,203 +21,232 @@
   flake.nixosModules.discord =
     {
       config,
+      pkgs,
       lib,
       ...
     }:
     let
       inherit (lib) mkIf;
+
+      openASARSettings =
+        {
+          DANGEROUS_ENABLE_DEVTOOLS_ONLY_ENABLE_IF_YOU_KNOW_WHAT_YOURE_DOING = true;
+          trayBalloonShown = false;
+          # TODO: Use this when theming
+          # BACKGROUND_COLOR = "";
+          # TODO: This is a useful feature but it doesn't work with the proot bind mounts
+          SKIP_HOST_UPDATE = true;
+        }
+        |> (pkgs.formats.json { }).generate "openasar-settings.json";
+
+      vencordSettings =
+        {
+          autoUpdate = false;
+          autoUpdateNotification = false;
+          notifyAboutUpdates = false;
+
+          disableMinSize = true;
+          enableReactDevtools = false;
+
+          useQuickCSS = true;
+
+          frameless = true;
+          transparent = false;
+
+          plugins = {
+            BetterGifPicker.enabled = true;
+            BetterSettings = {
+              disableFade = true;
+              eagerLoad = true;
+              enabled = true;
+              organizeMenu = true;
+            };
+            BetterUploadButton.enabled = true;
+            ChatInputButtonAPI.enabled = true;
+            CommandsAPI.enabled = true;
+            ConsoleJanitor = {
+              disableLoggers = true;
+              disableNoisyLoggers = false;
+              disableSpotifyLogger = true;
+              enabled = true;
+              whitelistedLoggers = "GatewaySocket; Routing/Utils";
+            };
+            CrashHandler = {
+              attemptToNavigateToHome = false;
+              attemptToPreventCrashes = true;
+              enabled = true;
+            };
+            Experiments = {
+              enabled = true;
+              toolbarDevMenu = false;
+            };
+            FakeNitro = {
+              disableEmbedPermissionCheck = false;
+              emojiSize = 48;
+              enableEmojiBypass = true;
+              enableStickerBypass = true;
+              enableStreamQualityBypass = true;
+              enabled = true;
+              hyperLinkText = "{{NAME}}";
+              stickerSize = 160;
+              transformCompoundSentence = false;
+              transformEmojis = true;
+              transformStickers = true;
+              useHyperLinks = true;
+            };
+            FavoriteGifSearch = {
+              enabled = true;
+              searchOption = "hostandpath";
+            };
+            ImageZoom = {
+              enabled = true;
+              invertScroll = true;
+              nearestNeighbour = true;
+              saveZoomValues = true;
+              size = 200;
+              square = true;
+              zoom = 1;
+              zoomSpeed = 0.5;
+            };
+            LoadingQuotes = {
+              additionalQuotes = "";
+              additionalQuotesDelimiter = "|";
+              enableDiscordPresetQuotes = false;
+              enablePluginPresetQuotes = true;
+              enabled = true;
+              replaceEvents = true;
+            };
+            MemberListDecoratorsAPI.enabled = true;
+            MessageAccessoriesAPI.enabled = true;
+            MessageClickActions = {
+              enableDeleteOnClick = false;
+              enableDoubleClickToEdit = true;
+              enableDoubleClickToReply = true;
+              enabled = true;
+              requireModifier = false;
+            };
+            MessageDecorationsAPI.enabled = true;
+            MessageEventsAPI.enabled = true;
+            MessageLatency = {
+              detectDiscordKotlin = true;
+              enabled = true;
+              latency = 2;
+              showMillis = false;
+            };
+            MessageLogger = {
+              collapseDeleted = false;
+              deleteStyle = "text";
+              enabled = true;
+              ignoreBots = false;
+              ignoreChannels = "";
+              ignoreGuilds = "";
+              ignoreSelf = false;
+              ignoreUsers = "";
+              inlineEdits = true;
+              logDeletes = true;
+              logEdits = true;
+            };
+            MessagePopoverAPI.enabled = true;
+            MessageUpdaterAPI.enabled = true;
+            NoTrack = {
+              disableAnalytics = true;
+              enabled = true;
+            };
+            NoTypingAnimation.enabled = true;
+            NoUnblockToJump.enabled = true;
+            OnePingPerDM = {
+              allowEveryone = false;
+              allowMentions = false;
+              channelToAffect = "both_dms";
+              enabled = true;
+            };
+            PermissionFreeWill = {
+              enabled = true;
+              lockout = true;
+              onboarding = true;
+            };
+            PermissionsViewer = {
+              defaultPermissionsDropdownState = false;
+              enabled = true;
+              permissionsSortOrder = 0;
+            };
+            ServerListAPI.enabled = true;
+            Settings = {
+              enabled = true;
+              settingsLocation = "aboveNitro";
+            };
+            ShowHiddenChannels = {
+              enabled = true;
+              hideUnreads = true;
+              showHiddenChannels = true;
+              showMode = 0;
+            };
+            SilentTyping = {
+              contextMenu = true;
+              enabled = true;
+              isEnabled = true;
+              showIcon = false;
+            };
+            SpotifyCrack = {
+              enabled = true;
+              keepSpotifyActivityOnIdle = false;
+              noSpotifyAutoPause = true;
+            };
+            SupportHelper.enabled = true;
+            ThemeAttributes.enabled = true;
+            UserSettingsAPI.enabled = true;
+            ValidUser.enabled = true;
+            ViewRaw = {
+              clickMethod = "Left";
+              enabled = true;
+            };
+            WebContextMenus.enabled = true;
+            WebScreenShareFixes.enabled = true;
+            YoutubeAdblock.enabled = true;
+            ExpressionCloner.enabled = true;
+            BadgeAPI.enabled = true;
+          };
+        }
+        |> (pkgs.formats.json { }).generate "vencord-settings.json";
+
+      package = config.prefs.discord.package;
+      wrapped = pkgs.stdenv.mkDerivation {
+        pname = "discord-wrapped";
+        inherit (package) version;
+
+        buildInputs = [
+          pkgs.proot
+          package
+        ];
+
+        dontUnpack = true;
+        dontBuild = true;
+
+        installPhase =
+          let
+            home = "/home/${config.prefs.user.name}";
+          in
+          # bash
+          ''
+            mkdir -p $out/bin
+            cat > $out/bin/discord <<EOF
+            #!${pkgs.bash}/bin/bash
+            exec ${pkgs.proot}/bin/proot \
+              -b ${openASARSettings}:${home}/.config/discord/settings.json \
+              -b ${vencordSettings}:${home}/.config/Vencord/settings/settings.json \
+              ${pkgs.discord}/bin/discord --no-sandbox "\$@"
+            EOF
+            chmod +x $out/bin/discord
+          '';
+
+        inherit (package) meta;
+      };
     in
     {
       environment.systemPackages = [
-        config.prefs.discord.package
+        wrapped
       ];
 
       prefs.autostart.discord = mkIf config.prefs.discord.autostart config.prefs.discord.package;
-
-      hjem.users.${config.prefs.user.name} = {
-        # Discord config
-        xdg.config.files."discord/settings.json" = {
-          type = "copy";
-          generator = lib.generators.toJSON { };
-          value = {
-            DANGEROUS_ENABLE_DEVTOOLS_ONLY_ENABLE_IF_YOU_KNOW_WHAT_YOURE_DOING = true;
-            trayBalloonShown = false;
-            # TODO: Use this when theming
-            # BACKGROUND_COLOR = "";
-          };
-        };
-
-        xdg.config.files."Vencord/settings/settings.json" = {
-          type = "copy";
-          generator = lib.generators.toJSON { };
-          value = {
-            autoUpdate = false;
-            autoUpdateNotification = false;
-            notifyAboutUpdates = false;
-
-            disableMinSize = true;
-            enableReactDevtools = false;
-
-            useQuickCSS = true;
-
-            frameless = true;
-            transparent = false;
-
-            plugins = {
-              BetterGifPicker.enabled = true;
-              BetterSettings = {
-                disableFade = true;
-                eagerLoad = true;
-                enabled = true;
-                organizeMenu = true;
-              };
-              BetterUploadButton.enabled = true;
-              ChatInputButtonAPI.enabled = true;
-              CommandsAPI.enabled = true;
-              ConsoleJanitor = {
-                disableLoggers = true;
-                disableNoisyLoggers = false;
-                disableSpotifyLogger = true;
-                enabled = true;
-                whitelistedLoggers = "GatewaySocket; Routing/Utils";
-              };
-              CrashHandler = {
-                attemptToNavigateToHome = false;
-                attemptToPreventCrashes = true;
-                enabled = true;
-              };
-              Experiments = {
-                enabled = true;
-                toolbarDevMenu = false;
-              };
-              FakeNitro = {
-                disableEmbedPermissionCheck = false;
-                emojiSize = 48;
-                enableEmojiBypass = true;
-                enableStickerBypass = true;
-                enableStreamQualityBypass = true;
-                enabled = true;
-                hyperLinkText = "{{NAME}}";
-                stickerSize = 160;
-                transformCompoundSentence = false;
-                transformEmojis = true;
-                transformStickers = true;
-                useHyperLinks = true;
-              };
-              FavoriteGifSearch = {
-                enabled = true;
-                searchOption = "hostandpath";
-              };
-              ImageZoom = {
-                enabled = true;
-                invertScroll = true;
-                nearestNeighbour = true;
-                saveZoomValues = true;
-                size = 200;
-                square = true;
-                zoom = 1;
-                zoomSpeed = 0.5;
-              };
-              LoadingQuotes = {
-                additionalQuotes = "";
-                additionalQuotesDelimiter = "|";
-                enableDiscordPresetQuotes = false;
-                enablePluginPresetQuotes = true;
-                enabled = true;
-                replaceEvents = true;
-              };
-              MemberListDecoratorsAPI.enabled = true;
-              MessageAccessoriesAPI.enabled = true;
-              MessageClickActions = {
-                enableDeleteOnClick = false;
-                enableDoubleClickToEdit = true;
-                enableDoubleClickToReply = true;
-                enabled = true;
-                requireModifier = false;
-              };
-              MessageDecorationsAPI.enabled = true;
-              MessageEventsAPI.enabled = true;
-              MessageLatency = {
-                detectDiscordKotlin = true;
-                enabled = true;
-                latency = 2;
-                showMillis = false;
-              };
-              MessageLogger = {
-                collapseDeleted = false;
-                deleteStyle = "text";
-                enabled = true;
-                ignoreBots = false;
-                ignoreChannels = "";
-                ignoreGuilds = "";
-                ignoreSelf = false;
-                ignoreUsers = "";
-                inlineEdits = true;
-                logDeletes = true;
-                logEdits = true;
-              };
-              MessagePopoverAPI.enabled = true;
-              MessageUpdaterAPI.enabled = true;
-              NoTrack = {
-                disableAnalytics = true;
-                enabled = true;
-              };
-              NoTypingAnimation.enabled = true;
-              NoUnblockToJump.enabled = true;
-              OnePingPerDM = {
-                allowEveryone = false;
-                allowMentions = false;
-                channelToAffect = "both_dms";
-                enabled = true;
-              };
-              PermissionFreeWill = {
-                enabled = true;
-                lockout = true;
-                onboarding = true;
-              };
-              PermissionsViewer = {
-                defaultPermissionsDropdownState = false;
-                enabled = true;
-                permissionsSortOrder = 0;
-              };
-              ServerListAPI.enabled = true;
-              Settings = {
-                enabled = true;
-                settingsLocation = "aboveNitro";
-              };
-              ShowHiddenChannels = {
-                enabled = true;
-                hideUnreads = true;
-                showHiddenChannels = true;
-                showMode = 0;
-              };
-              SilentTyping = {
-                contextMenu = true;
-                enabled = true;
-                isEnabled = true;
-                showIcon = false;
-              };
-              SpotifyCrack = {
-                enabled = true;
-                keepSpotifyActivityOnIdle = false;
-                noSpotifyAutoPause = true;
-              };
-              SupportHelper.enabled = true;
-              ThemeAttributes.enabled = true;
-              UserSettingsAPI.enabled = true;
-              ValidUser.enabled = true;
-              ViewRaw = {
-                clickMethod = "Left";
-                enabled = true;
-              };
-              WebContextMenus.enabled = true;
-              WebScreenShareFixes.enabled = true;
-              YoutubeAdblock.enabled = true;
-              ExpressionCloner.enabled = true;
-              BadgeAPI.enabled = true;
-            };
-          };
-        };
-      };
     };
 }
