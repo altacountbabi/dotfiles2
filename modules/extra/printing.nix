b93465d753e05fcb1705961d33981b99118255b3
altacountbabi <altacountbabi@users.noreply.github.com>
2025-11-22 23:55:01 +0200

    Add support for scanners with SANE in printing module

--- modules/extra/printing.nix
+++ modules/extra/printing.nix
@@ -10,15 +10,28 @@
           gutenprint
           hplip
         ]) "List of printer drivers to install";
+
+        scanning = {
+          printers = mkOpt (types.listOf types.str) "" "List of IPs for network scanners";
+          backends = mkOpt (types.listOf types.package) (with pkgs; [
+            airscan
+          ]) "List of SANE backends to install";
+        };
       };
     };
 
   flake.nixosModules.printing =
-    { config, ... }:
+    { config, lib, ... }:
     {
       services.printing = {
         enable = true;
         inherit (config.prefs.printing) drivers;
+      };
+
+      hardware.sane = {
+        enable = lib.mkDefault true;
+        netConf = config.prefs.scanning.printers |> builtins.concatStringsSep "\n";
+        extraBackends = config.prefs.scanning.backends;
       };
     };
 }
