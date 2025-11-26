3d0d4fda4badf35d3bbc23fdaae29178886173af
altacountbabi <altacountbabi@users.noreply.github.com>
2025-11-26 02:18:26 +0200

    Add option to enable `i2c-dev` in kernel module

--- modules/base/kernel.nix
+++ modules/base/kernel.nix
@@ -7,16 +7,25 @@
     opts =
       { mkOpt, types, ... }:
       {
-        latest = mkOpt types.bool true "Whether to use the latest kernel or the LTS ";
+        latest = mkOpt types.bool true "Whether to use the latest kernel or the LTS";
         params = mkOpt (types.listOf types.str) [ ] "Parameters added to the kernel command line.";
+
+        enableI2C = mkOpt types.bool true "Whether to enable the `i2c-dev` kernel module";
       };
 
     cfg =
-      { cfg, pkgs, ... }:
+      {
+        pkgs,
+        lib,
+        cfg,
+        ...
+      }:
       {
         boot = {
           kernelPackages = if cfg.latest then pkgs.linuxPackages_latest else pkgs.linuxPackages;
           kernelParams = cfg.params;
+
+          kernelModules = (lib.optional cfg.enableI2C "i2c-dev");
         };
       };
   };
