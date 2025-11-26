e15f8e70da8b091f463562668db8e42e7d4e1102
altacountbabi <altacountbabi@users.noreply.github.com>
2025-11-26 02:18:26 +0200

    Add wake on lan support in networking module

--- modules/base/network.nix
+++ modules/base/network.nix
@@ -8,13 +8,32 @@
       { mkOpt, types, ... }:
       {
         hostname = mkOpt types.str "nixos" "The name of the host";
+        wol = mkOpt (types.nullOr types.str) null "Name of interface to enable wake on lan for";
       };
 
     cfg =
-      { cfg, ... }:
       {
+        pkgs,
+        lib,
+        cfg,
+        ...
+      }:
+      {
+        prefs.user.groups = [ "networkmanager" ];
+
         networking.hostName = cfg.hostname;
         networking.networkmanager.enable = true;
+
+        systemd.services.enable-wol = lib.mkIf (cfg.wol != null) {
+          description = "Enable Wake-on-LAN";
+          wantedBy = [ "multi-user.target" ];
+          after = [ "network.target" ];
+
+          serviceConfig = {
+            Type = "oneshot";
+            ExecStart = [ "${pkgs.ethtool |> lib.getExe} -s ${cfg.wol} wol g" ];
+          };
+        };
       };
   };
 }
