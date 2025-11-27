8cc07dd085b0fc5a906927b22c7b5749fa21b049
altacountbabi <altacountbabi@users.noreply.github.com>
2025-11-27 23:27:15 +0200

    Add public key config in ssh module

--- modules/extra/ssh.nix
+++ modules/extra/ssh.nix
@@ -1,7 +1,24 @@
+{ self, ... }:
+
 {
-  flake.nixosModules.ssh =
-    { ... }:
-    {
-      services.openssh.enable = true;
-    };
+  flake.nixosModules = self.mkModule "ssh" {
+    path = "ssh";
+
+    opts =
+      { mkOpt, types, ... }:
+      {
+        pubKeys = mkOpt (types.listOf types.str) [ ] "List of public ssh keys to authorize";
+      };
+
+    cfg =
+      { config, cfg, ... }:
+      {
+        users.users.${config.prefs.user.name}.openssh.authorizedKeys.keys = cfg.pubKeys;
+
+        services.openssh = {
+          enable = true;
+          settings.PasswordAuthentication = (builtins.length cfg.pubKeys) != 0;
+        };
+      };
+  };
 }
