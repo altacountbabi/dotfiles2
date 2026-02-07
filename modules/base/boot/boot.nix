d378af936f514a7911e5805b214a68d5385f0c82
altacountbabi <altacountbabi@users.noreply.github.com>
2026-02-07 21:56:10 +0200

    Add better support for vm testing with the `vmVariant` option

--- modules/base/boot/boot.nix
+++ modules/base/boot/boot.nix
@@ -12,6 +12,22 @@
       system.etc.overlay.enable = true;
       system.nixos-init.enable = true;
 
+      virtualisation.vmVariant = {
+        # Nixpkgs issue
+        system.nixos-init.enable = lib.mkForce false;
+
+        virtualisation = {
+          cores = 8;
+          memorySize = 8 * 1024;
+          tpm.enable = true;
+          qemu.options = [
+            "-device virtio-gpu-gl"
+            "-display gtk,show-menubar=off,zoom-to-fit=off,gl=on"
+            "-vga virtio"
+          ];
+        };
+      };
+
       # Removes the need to import `not-detected.nix` in every host
       hardware.enableRedistributableFirmware = lib.mkDefault true;
     };
