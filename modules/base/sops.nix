27e36f1e42f672d7600f30c71d130d80a9ae8215
altacountbabi <altacountbabi@users.noreply.github.com>
2026-02-10 16:15:14 +0200

    Install sops cli in sops module

--- modules/base/sops.nix
+++ modules/base/sops.nix
@@ -2,20 +2,26 @@
 
 {
   flake.nixosModules = self.mkModule {
-    cfg = _: {
-      imports = [
-        inputs.sops.nixosModules.sops
-      ];
+    cfg =
+      { pkgs, ... }:
+      {
+        imports = [
+          inputs.sops.nixosModules.sops
+        ];
 
-      config = {
-        sops = {
-          defaultSopsFile = ../../secrets/secrets.yaml;
-          age = {
-            keyFile = "/var/lib/sops-nix/key.txt";
-            generateKey = true;
+        config = {
+          sops = {
+            defaultSopsFile = ../../secrets/secrets.yaml;
+            age = {
+              keyFile = "/var/lib/sops-nix/key.txt";
+              generateKey = true;
+            };
           };
+
+          environment.systemPackages = with pkgs; [
+            sops
+          ];
         };
       };
-    };
   };
 }
