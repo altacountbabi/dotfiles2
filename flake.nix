b50f1d5056022622a17b48a1d8dcab31c71b143e
Whoman <altacountbabi@users.noreply.github.com>
2026-03-14 23:52:20 +0200

    Re-organize flake inputs

--- flake.nix
+++ flake.nix
@@ -1,40 +1,53 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
     nur.url = "github:nix-community/nur";
-    nix-on-droid.url = "github:altacountbabi/nix-on-droid";
+    nur.inputs.nixpkgs.follows = "nixpkgs";
+
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
 
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     wrappers.url = "github:lassulus/wrappers";
+    wrappers.inputs.nixpkgs.follows = "nixpkgs";
 
-    # package name completions in shells
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+
+    sops.url = "github:Mic92/sops-nix";
+    sops.inputs.nixpkgs.follows = "nixpkgs";
+
+    dms.url = "github:AvengeMedia/DankMaterialShell";
+    dms.inputs.nixpkgs.follows = "nixpkgs";
+
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    zen-browser.inputs.nixpkgs.follows = "nixpkgs";
+
+    helix.url = "github:altacountbabi/helix/feat/file-manager";
+    helix.inputs.nixpkgs.follows = "nixpkgs";
+
+    declarative-jellyfin.url = "github:altacountbabi/declarative-jellyfin";
+    declarative-jellyfin.inputs.nixpkgs.follows = "nixpkgs";
+
+    opencode.url = "github:sst/opencode";
+    opencode.inputs.nixpkgs.follows = "nixpkgs";
+
+    mcp-nixos.url = "github:utensils/mcp-nixos";
+    mcp-nixos.inputs.nixpkgs.follows = "nixpkgs";
+
+    noogle.url = "github:nix-community/noogle";
+    noogle.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-on-droid.url = "github:altacountbabi/nix-on-droid";
+    nix-on-droid.inputs.nixpkgs.follows = "nixpkgs";
+
+    microfetch.url = "github:NotAShelf/microfetch";
+    microfetch.inputs.nixpkgs.follows = "nixpkgs";
+
     nix-index = {
       url = "github:altacountbabi/dotfiles2/nix-index";
       flake = false;
     };
-    noogle.url = "github:nix-community/noogle";
-
-    sops = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # Apps
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
-    dms.url = "github:AvengeMedia/DankMaterialShell";
-    helix.url = "github:altacountbabi/helix/feat/file-manager";
-    opencode.url = "github:sst/opencode";
-    microfetch.url = "github:NotAShelf/microfetch";
-
-    mcp-nixos.url = "github:utensils/mcp-nixos";
-
-    declarative-jellyfin.url = "github:altacountbabi/declarative-jellyfin";
   };
 
   outputs =
