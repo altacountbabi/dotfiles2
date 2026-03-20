a6c2a1f8c2ae1b8c42be7c4928490c070f3bf93b
Whoman <altacountbabi@users.noreply.github.com>
2026-03-20 12:58:33 +0200

    Fix sddm not launching

--- modules/extra/graphics/greeters/sddm.nix
+++ modules/extra/graphics/greeters/sddm.nix
@@ -6,7 +6,6 @@
 
     cfg =
       {
-        config,
         pkgs,
         lib,
         cfg,
@@ -19,53 +18,6 @@
           services.displayManager.sddm = {
             package = lib.mkDefault pkgs.kdePackages.sddm;
             wayland.enable = true;
-            settings = {
-              Wayland.CompositorCommand =
-                let
-                  westonTransformNames = {
-                    "normal" = "normal";
-                    "90" = "rotate-90";
-                    "180" = "rotate-180";
-                    "270" = "rotate-270";
-                    "flipped" = "flipped";
-                    "flipped-90" = "flipped-rotate-90";
-                    "flipped-180" = "flipped-rotate-180";
-                    "flipped-270" = "flipped-rotate-270";
-                  };
-                  monitors = config.prefs.monitors |> lib.filterAttrs (_: x: x.enable);
-
-                  westonConfig = lib.generators.toINI { listsAsDuplicateKeys = true; } {
-                    output = {
-                      name = lib.attrNames monitors;
-                      mode =
-                        lib.attrValues monitors
-                        |> map (x: "${toString x.width}x${toString x.height}@${lib.strings.floatToString x.refreshRate}");
-                      scale = lib.attrValues monitors |> map (x: x.scale);
-                      transform =
-                        lib.attrValues monitors |> map (x: westonTransformNames.${toString x.transform} or "normal");
-                    };
-
-                    keyboard = {
-                      keymap_layout = "us";
-                      keymap_model = "pc104";
-                      keymap_options = "terminate:ctrl_alt_bksp";
-                    };
-
-                    libinput = {
-                      enable-tap = true;
-                      left-handed = false;
-                      accel-profile = "flat";
-                    };
-
-                    # TODO: Figure out xcursor issues
-                    # shell = {
-                    #   cursor-theme = "Adwaita";
-                    #   cursor-size = 24;
-                    # };
-                  };
-                in
-                "${pkgs.weston}/bin/weston --shell=kiosk -c ${builtins.toFile "weston.ini" westonConfig}";
-            };
           };
         };
       };
