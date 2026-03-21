bcbfe72602f7f9055146e438a55f688e5517caac
Whoman <altacountbabi@users.noreply.github.com>
2026-03-21 23:14:13 +0200

    Use newer version of plymouth (for the love of god can they make an official release for this)

--- modules/extra/plymouth.nix
+++ modules/extra/plymouth.nix
@@ -1,8 +1,27 @@
 {
   flake.nixosModules.base =
-    { config, lib, ... }:
+    {
+      config,
+      pkgs,
+      lib,
+      ...
+    }:
     {
       boot = lib.mkIf config.boot.plymouth.enable {
+        plymouth.package =
+          (pkgs.plymouth.override {
+            systemd = config.boot.initrd.systemd.package;
+          }).overrideAttrs
+            {
+              src = pkgs.fetchFromGitLab {
+                domain = "gitlab.freedesktop.org";
+                owner = "plymouth";
+                repo = "plymouth";
+                rev = "082c606b4306afc7f16fdddd0909ef006e812f98";
+                hash = "sha256-8HIWYqzR+ovxlAmch0SvQdFFm9t1aw+h9jam/mSJx9E=";
+              };
+            };
+
         consoleLogLevel = 0;
         initrd.verbose = false;
         kernelParams = [
